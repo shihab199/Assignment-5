e79a94bb8f735d6dc286e76a8599ef2725ea91c2
shihab199 <mamunhasan.shihab20@gmail.com>
2023-08-27 13:48:21 +0600

    all updated

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-const applyBtn= document.getElementById('apply-btn')
-const itemPrice = [];
-const calculateDiscount=document.getElementById('discount')
-
-
 function productAddToCart(name,price){
     const prodName = document.getElementById(name).innerHTML;
     const prodPrice= document.getElementById(price).innerHTML;
    
     const prodPriceFloat = parseFloat(prodPrice);
     const roundedPrice = Number(prodPriceFloat.toFixed(2));
+ 
     setItemsCart(prodName);
     setItemsCartPrice(roundedPrice)
+    setItemsCartPrice(itemPrice)
 }
 
-// Add to card items name entry
-function setItemsCart(itemName){
-    const itemsCard= document.getElementById('cart-items');
-    const count= itemsCard.childElementCount;
-        const p=document.createElement('p');
-        // p.classList.add('');
-        // p.classList.add('');
-        p.innerHTML= `${count+1}. ${itemName}`
-        p.classList.add('text-[24px]')
-        p.classList.add('font-medium')
-        itemsCard.appendChild(p);
-    }
 
-    
-    // Add to card items price entry
+
+
+function setItemsCart(itemName){
+const itemsCard= document.getElementById('cart-items');
+const count= itemsCard.childElementCount;
+    const p=document.createElement('p');
+    p.innerHTML= `${count+1}. ${itemName}`
+    p.classList.add('text-[24px]')
+    p.classList.add('font-medium')
+    itemsCard.appendChild(p);
+}
+
 let totalPrice = 0;
 function setItemsCartPrice(itemPrice) {
     itemPrices.push(itemPrice);
-    totalPrice = itemPrices.reduce((sum, price) => sum + price, 0);
+    totalPrice = itemPrice.reduce((sum, price) => sum + price, 0);
     const itemsPriceCard = document.getElementById('total-price');
     itemsPriceCard.innerHTML = totalPrice.toFixed(2);
 
@@ -40,7 +35,7 @@ function setItemsCartPrice(itemPrice) {
     if (totalPrice > 0) {
         makePurchaseBtn.removeAttribute('disabled');
     }
-    // total
+
 const total= totalPrice;
 
 
@@ -49,9 +44,6 @@ totalCost.innerHTML= total.toFixed(2);
 
 
 }
-
-
-// Enables coupon cart button
 
 
 const couponCode = document.getElementById('coupon-code').addEventListener('keyup', function(event){
@@ -63,13 +55,14 @@ const couponCode = document.getElementById('coupon-code').addEventListener('keyu
 })
 
 
-//Apply discount
 let discount=0;
 function generateDiscount(){
     discount= totalPrice*0.2;
     calculateDiscount.innerHTML= discount.toFixed(2);
-       //if discount is applied, set total
+
+
 const total= totalPrice-discount;
 const totalCost= document.getElementById('total');
 totalCost.innerHTML= total.toFixed(2);
 }
+
